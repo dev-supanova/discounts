aaed2b7b2b29b9c33806afd11b5fa874b565c3ad
Alec Agisanyang <aagisanyang@bitri.co.bw>
2021-11-14 23:01:20 +0200

    Default spring initializr project

---
+++ src/test/java/xyz/geniuslaec/discounts/DiscountsApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.geniuslaec.discounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
